e2ba5e082c3a16b238856c4b5bce930ba8459966
Muhammad Alfi <125952078+allcuy@users.noreply.github.com>
2023-06-18 09:07:40 +0700

    tugasaya

---
+++ page/list_barang.php
@@ -0,0 +1,35 @@
+<h2>
+    DAFTAR INVENTARIS
+</h2>
+<a href="" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-plus">Tambah BArang</span></a>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Kode Inventaris</th>
+            <th>Nama Barang</th>
+            <th>Kondisi</th>
+            <th>Jumlah</th>
+            <th>Ruang</th>
+            <th>Tanggal Regester</th>
+            <th>Keterangan</th>
+            <th>Opsi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>001</td>
+            <td>Lebtop</td>
+            <td>Baik</td>
+            <td>50</td>
+            <td>LAB RPL</td>
+            <td>06-06-2023</td>
+            <td>Barang di dapat dari bantuan provensi</td>
+            <td>
+                <a href="" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
+                <a href="" class="btn btn-md btn-danger"><span class="glyphicon glyphicon-trash "></span></a>
+            </td>
+        </tr>
+    </tbody>
+</table>
